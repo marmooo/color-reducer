864edff0a9d163610f59ea1aa76ca9447d91059e
marmooo <marmooo@users.noreply.github.com>
2024-07-28 11:02:44 +0900

    fix path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Tooltip } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/+esm";
 import imageCompareViewer from "https://cdn.jsdelivr.net/npm/image-compare-viewer@1.6.2/+esm";
-import { encode } from "/color-reducer/pngs.js";
+import { encode } from "./pngs.js";
 
 function loadConfig() {
   if (localStorage.getItem("darkMode") == 1) {
