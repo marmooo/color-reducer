79681682dd162e3b1062b6e197daea3fddcab45a
marmooo <marmooo@users.noreply.github.com>
2026-01-03 07:23:25 +0900

    bump opencv.js from 4.12.0 to 4.13.0

--- build_opencvjs.sh
+++ build_opencvjs.sh
@@ -1,13 +1,7 @@
-# This build script is designed to be used with the following build script.
-#   https://github.com/marmooo/mini-web-apps/blob/main/opencv/embindgen.py
-# It has the ability to cache builds and minimize output.
-# You can use it by replacing the original script below.
-#   ${opencv_dir}/modules/js/generator/embindgen.py
-# You can also build wasm without my build script, but reconfigure cmake_options.
 base_dir=${HOME}/workspace
-emsdk_dir=${base_dir}/emsdk  # 3.1.74
-opencv_dir=${base_dir}/opencv  # 4.11.0
-opencv_contrib_dir=${base_dir}/opencv_contrib  # 4.11.0
+emsdk_dir=${base_dir}/emsdk  # 4.0.19
+opencv_dir=${base_dir}/opencv  # 4.13.0
+opencv_contrib_dir=${base_dir}/opencv_contrib  # 4.13.0
 build_py=${opencv_dir}/platforms/js/build_js.py
 build_wasm_dir=${opencv_dir}/build_wasm
 build_simd_dir=${opencv_dir}/build_simd
@@ -26,10 +20,10 @@ options="\
   --cmake_option=-DBUILD_opencv_xphoto=ON \
   --cmake_option=-DBUILD_opencv_ximgproc=ON \
   --cmake_option=-DBUILD_EXAMPLES=OFF \
-  --cmake_option=-DOPENCV_EXTRA_MODULES_PATH=${opencv_contrib_dir}/modules \
   --config whitelist.json \
   --disable_single_file \
   --opencv_dir ${opencv_dir} \
+  --extra_modules ${opencv_contrib_dir}/modules \
   --emscripten_dir ${emsdk_dir}/upstream/emscripten"
 
 source ${emsdk_dir}/emsdk_env.sh
